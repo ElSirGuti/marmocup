eda385f75c756abab76a079bb77068c77a253d47
Andres Gutierrez <87587118+ElSirGuti@users.noreply.github.com>
2025-07-14 23:14:24 -0400

    actualizaciones correctas

--- public/js/calendario.js
+++ public/js/calendario.js
@@ -15,12 +15,134 @@ document.addEventListener('DOMContentLoaded', function() {
             {
                 id: 'evento-2',
                 imagen: 'img/circuitos/sugo.avif',
-                torneo: 'MarmoCup Sim Boxer 2025',
+                torneo: 'Super Formula Virtual 2025',
                 ronda: 'Ronda 5: Sportsland SUGO',
                 plataforma: 'Assetto Corsa',
-                fecha: '10 Julio 2025',
+                fecha: '17 Julio 2025',
                 hora: '20:00 GMT-4',
                 estado: 'Próximo'
+            },
+            {
+                id: 'evento-3',
+                imagen: 'img/circuitos/spa-pcup.jpg',
+                torneo: 'Porsche Mobil 1 Super Cup Virtual 2025',
+                ronda: 'Ronda 5: Spa Francorchamps',
+                plataforma: 'Assetto Corsa',
+                fecha: '18 Julio 2025',
+                hora: '20:00 GMT-4',
+                estado: 'Próximo'
+            },
+            {
+                id: 'evento-4',
+                imagen: 'img/circuitos/bilsterbergmx5.png',
+                torneo: 'MarmoCup Track Toys Challenge 2025',
+                ronda: 'Ronda 3: Bilster Berg',
+                plataforma: 'Assetto Corsa',
+                fecha: '19 Julio 2025',
+                hora: '20:00 GMT-4',
+                estado: 'Próximo'
+            },
+            {
+                "id": "evento-5",
+                "imagen": "img/circuitos/fuji-sf.jpg",
+                "torneo": "Super Formula Virtual 2025",
+                "ronda": "Ronda 6: Fuji Speedway",
+                "plataforma": "Assetto Corsa",
+                "fecha": "24 Julio 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            },
+            {
+                "id": "evento-8",
+                "imagen": "img/circuitos/hungaroring.jpg",
+                "torneo": "Porsche Mobil 1 Super Cup Virtual 2025",
+                "ronda": "Ronda 6: Hungaroring",
+                "plataforma": "Assetto Corsa",
+                "fecha": "25 Julio 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            },
+            {
+                "id": "evento-9",
+                "imagen": "img/circuitos/okutama.png",
+                "torneo": "Track Toys Challenge 2025",
+                "ronda": "Ronda 4: Okutama",
+                "plataforma": "Assetto Corsa",
+                "fecha": "26 Julio 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            },
+            {
+                "id": "evento-10",
+                "imagen": "img/circuitos/suzuka.webp",
+                "torneo": "Super Formula Virtual 2025",
+                "ronda": "Ronda 7: Suzuka",
+                "plataforma": "Assetto Corsa",
+                "fecha": "31 Julio 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            }
+        ],
+        'agosto-2025': [
+            {
+                "id": "evento-2",
+                "imagen": "img/circuitos/zandvoort.jpeg",
+                "torneo": "Porsche Mobil 1 Super Cup Virtual 2025",
+                "ronda": "Ronda 7: Zandvoort",
+                "plataforma": "Assetto Corsa",
+                "fecha": "1 Agosto 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            },
+            {
+                "id": "evento-3",
+                "imagen": "img/circuitos/bikernieki.png",
+                "torneo": "Track Toys Challenge 2025",
+                "ronda": "Ronda 5: Bikernieki",
+                "plataforma": "Assetto Corsa",
+                "fecha": "2 Agosto 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            },
+            {
+                "id": "evento-4",
+                "imagen": "img/circuitos/monza.jpeg",
+                "torneo": "Porsche Mobil 1 Super Cup Virtual 2025",
+                "ronda": "Ronda 8: Monza",
+                "plataforma": "Assetto Corsa",
+                "fecha": "8 Agosto 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            },
+            {
+                "id": "evento-5",
+                "imagen": "img/circuitos/camino.png",
+                "torneo": "Track Toys Challenge 2025",
+                "ronda": "Ronda 6: Camino Viejo",
+                "plataforma": "Assetto Corsa",
+                "fecha": "9 Agosto 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            },
+            {
+                "id": "evento-6",
+                "imagen": "img/circuitos/praga.png",
+                "torneo": "Track Toys Challenge 2025",
+                "ronda": "Ronda 7: Praga",
+                "plataforma": "Assetto Corsa",
+                "fecha": "16 Agosto 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
+            },
+            {
+                "id": "evento-7",
+                "imagen": "img/circuitos/pau.png",
+                "torneo": "Track Toys Challenge 2025",
+                "ronda": "Ronda 8: Circuit de PAU",
+                "plataforma": "Assetto Corsa",
+                "fecha": "23 Agosto 2025",
+                "hora": "20:00 GMT-4",
+                "estado": "Próximo"
             }
         ]
     };
